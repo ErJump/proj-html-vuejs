7dd26aa262afb439760302f00c028be55291389d
ErJump <fedelijunior@gmail.com>
2022-07-26 16:12:18 +0200

    feat: :sparkles: Adds OnlineCourses component
    Not styled yet

--- src/components/macro/ContentMain.vue
+++ src/components/macro/ContentMain.vue
@@ -2,17 +2,20 @@
   <div>
     <ImpressionsMain/>
     <AboutMe/>
+    <OnlineCourses/>
   </div>
 </template>
 
 <script>
 import ImpressionsMain from '../sections/ImpressionsMain.vue';
 import AboutMe from '../sections/AboutMe.vue';
+import OnlineCourses from '../sections/OnlineCourses.vue';
 export default {
   name: "ContentMain",
   components: {
     ImpressionsMain,
     AboutMe,
+    OnlineCourses
   }
 }
 </script>
---
+++ src/components/sections/OnlineCourses.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Courses</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OnlineCourses',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
