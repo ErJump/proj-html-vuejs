0e59cac477df0ce0fa8d7d6a337c96a57a206cd4
ErJump <fedelijunior@gmail.com>
2022-07-27 11:23:15 +0200

    feat: :sparkles: Completes RealStories component
    Adds goNext and goPrev methods and stories array to RealStories component

--- src/components/sections/RealStories.vue
+++ src/components/sections/RealStories.vue
@@ -4,19 +4,19 @@
       <div class="col-6 ms_left_column d-flex justify-content-center align-items-center">
         <div class="d-flex flex-column align-items-center">
           <h5 class="text-uppercase ms_secondary_color fs-6 mb-5 ms_brand_color">real stories</h5>
-          <span class="text-center fs-4 w-50 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, deleniti cumque, voluptas repellat assumenda culpa voluptates enim iure praesentium nihil sit amet veniam ratione optio pariatur? Numquam ad velit voluptatum!</span>
-          <img class="ms_avatar mb-4" src="../../../public/img/testimonial-avata-02.jpg" alt="avatar">
-          <span class="fs-5 mb-3">Mina Hollace</span>
-          <span>/freelancer</span>
+          <span class="text-center fs-4 w-50 mb-5">{{stories[activeElement].content}}</span>
+          <img class="ms_avatar mb-4" :src="completeImgPath(stories[activeElement].img)" :alt="stories[activeElement].img">
+          <span class="fs-5 mb-3">{{stories[activeElement].name}}</span>
+          <span>{{stories[activeElement].position}}</span>
         </div>
       </div>
       <div class="col-6 ms_right_column">
         <img src="../../../public/img/home-movation-testimonial-image.jpg" alt="testimonial-image">
       </div>
-      <div class="position-absolute ms_slider_menu d-flex flex-column justify-content-center">
-        <i class="fa-solid fa-caret-up ms_secondary_color"></i>
-        <span class="mb-2 mt-2">1/4</span>
-        <i class="fa-solid fa-caret-down ms_secondary_color"></i>
+      <div class="position-absolute ms_slider_menu d-flex flex-column justify-content-center align-items-center">
+        <i @click="goNext()" class="fa-solid fa-caret-up ms_secondary_color"></i>
+        <span class="mb-2 mt-2">{{activeElement + 1}}/4</span>
+        <i @click="goPrev()" class="fa-solid fa-caret-down ms_secondary_color"></i>
       </div>
     </div>
   </div>
@@ -25,6 +25,56 @@
 <script>
 export default {
   name: 'RealStories',
+  data: function() {
+    return {
+      activeElement: 0,
+      stories: [
+        {
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          name: "Mina Hollace",
+          img: "testimonial-avata-02.jpg",
+          position: "freelancer",
+        },
+        {
+          content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.",
+          name: "Jerry Maxwell", 
+          img: "testimonial-avata-01.jpg",
+          position: "designer",
+        },
+        {
+          content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+          name: "Walter White", 
+          img: "testimonial-avata-03.jpg",
+          position: "teacher",
+        },
+        {
+          content: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+          name: "Ted Green", 
+          img: "testimonial-avata-04.jpg",
+          position: "businessman",
+        },
+      ],
+    }
+  },
+  methods: {
+    completeImgPath: function(img) {
+      return `/img/${img}`;
+    },
+    goNext: function(){
+      if (this.activeElement < this.stories.length - 1) {
+        this.activeElement++;
+      } else {
+        this.activeElement = 0;
+      }
+    },
+    goPrev: function() {
+      if (this.activeElement > 0) {
+        this.activeElement--;
+      } else {
+        this.activeElement = this.stories.length - 1;
+      }
+    },
+  }
 }
 </script>
 
@@ -59,6 +109,12 @@ export default {
   transform: translate(-50px, -50px);
   &>*{
     text-align: center;
+  }
+  &>i{
+    cursor: pointer;
+    &:hover{
+      color: $brandColor;
+    }
   }
 }
 </style>
