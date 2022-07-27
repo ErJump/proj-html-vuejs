7bce940d8c6bdbacf90170f73d94ec54a3e21280
ErJump <fedelijunior@gmail.com>
2022-07-27 19:32:40 +0200

    feat: :sparkles: Adds allCourses functionality on button click to OnlineCourses component

--- src/components/sections/OnlineCourses.vue
+++ src/components/sections/OnlineCourses.vue
@@ -8,7 +8,7 @@
       <CourseCard v-for="card in courses" :key="card.id" :course="card"/>
     </div>
     <div class="w-100 d-flex justify-content-center">
-      <button class="ms_button w-25">
+      <button @click="getAllCourses()" class="ms_button w-25">
         View all courses
         <i class="fa-solid fa-arrow-right-long"></i>
       </button>
@@ -19,62 +19,95 @@
 <script>
 import CourseCard from '../commons/CourseCard.vue';
 export default {
-    name: "OnlineCourses",
-    components: { CourseCard },
-    data: function() {
-      return {
-        courses: [
-          {
-            title: "How to be BAD: Create A Growth Mindset For Toxicity",
-            image: "motivation-course-06-480x298.jpg",
-            cost: [30 , "00"],
-            lessons: 3,
-            students: 50,
-            id: 1,
-          },
-          {
-            title: "How to Build Confidence in Your Abilities",
-            image: "motivation-course-05-480x298.jpg",
-            cost: [30 , "00"],
-            lessons: 1,
-            students: 50,
-            id: 2,
-          },
-          {
-            title: "Fail Machine - Focus in a Distracted World",
-            image: "motivation-course-04-480x298.jpg",
-            cost: [20 , "00"],
-            lessons: 5,
-            students: 50,
-            id: 3,
-          },
-          {
-            title: "Effective Time Management Mastery - Complete Guide",
-            image: "motivation-course-03-480x298.jpg",
-            cost: [20 , "00"],
-            lessons: 18,
-            students: 50,
-            id: 4,
-          },
-          {
-            title: "Body Language Secrets Entrepreneurs",
-            image: "motivation-course-02-480x298.jpg",
-            cost: [25 , 99],
-            lessons: 19,
-            students: 50,
-            id: 5,
-          },
-          {
-            title: "Management Skills: The Science of a Jolly Cooperation",
-            image: "motivation-course-01-480x298.jpg",
-            cost: [19 , 99],
-            lessons: 17,
-            students: 50,
-            id: 6,
-          },
-        ]
-      }
+  name: "OnlineCourses",
+  components: { CourseCard },
+  data: function() {
+    return {
+      courses: [
+        {
+          title: "How to be BAD: Create A Growth Mindset For Toxicity",
+          image: "motivation-course-06-480x298.jpg",
+          cost: [30 , "00"],
+          lessons: 3,
+          students: 50,
+          id: 1,
+        },
+        {
+          title: "How to Build Confidence in Your Abilities",
+          image: "motivation-course-05-480x298.jpg",
+          cost: [30 , "00"],
+          lessons: 1,
+          students: 50,
+          id: 2,
+        },
+        {
+          title: "Fail Machine - Focus in a Distracted World",
+          image: "motivation-course-04-480x298.jpg",
+          cost: [20 , "00"],
+          lessons: 5,
+          students: 50,
+          id: 3,
+        },
+        {
+          title: "Effective Time Management Mastery - Complete Guide",
+          image: "motivation-course-03-480x298.jpg",
+          cost: [20 , "00"],
+          lessons: 18,
+          students: 50,
+          id: 4,
+        },
+        {
+          title: "Body Language Secrets Entrepreneurs",
+          image: "motivation-course-02-480x298.jpg",
+          cost: [25 , 99],
+          lessons: 19,
+          students: 50,
+          id: 5,
+        },
+        {
+          title: "Management Skills: The Science of a Jolly Cooperation",
+          image: "motivation-course-01-480x298.jpg",
+          cost: [19 , 99],
+          lessons: 17,
+          students: 50,
+          id: 6,
+        },
+      ],
+      allCourses: [
+        {
+          title: "Be a Nice Person",
+          image: "motivation-course-02-480x298.jpg",
+          cost: [1 , "00"],
+          lessons: 10,
+          students: 25,
+          id: 7,
+        },
+        {
+          title: "Enjoy Life Without Being Sigma",
+          image: "motivation-course-04-480x298.jpg",
+          cost: [25 , "00"],
+          lessons: 1,
+          students: 100,
+          id: 8,
+        },
+        {
+          title: "I'm Not In Danger, I Am The DANGER",
+          image: "motivation-course-05-480x298.jpg",
+          cost: [20 , "00"],
+          lessons: 1,
+          students: 60,
+          id: 9,
+        },
+      ],
     }
+  },
+  methods: {
+    getAllCourses: function() {
+      this.allCourses.forEach(course => {
+        this.courses.push(course);
+      });
+    },
+  },
 }
 </script>
 
