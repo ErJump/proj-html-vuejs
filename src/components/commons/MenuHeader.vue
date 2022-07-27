4d270ee53af843de9931e6541582aaf5fdad5df2
ErJump <fedelijunior@gmail.com>
2022-07-27 14:42:50 +0200

    feat: :sparkles: Adds img and postion fixed to MenuHeader component

--- src/components/commons/MenuHeader.vue
+++ src/components/commons/MenuHeader.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="position-absolute ms_menu shadow rounded d-flex flex-column gap-4 align-items-center">
-    <i class="fa-solid fa-ruler"></i>
-    <i class="fa-solid fa-circle-dot"></i>
-    <i class="fa-solid fa-book"></i>
-    <i class="fa-solid fa-cart-shopping"></i>
+  <div class="position-fixed ms_menu shadow rounded">
+    <div class="row">
+      <div class="col-2 d-flex flex-column gap-4 align-items-center justify-content-center">
+        <i class="fa-solid fa-ruler"></i>
+        <i class="fa-solid fa-circle-dot"></i>
+        <i class="fa-solid fa-book"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+      </div>
+      <div class="col-10 d-none">
+        <img class="img-fluid" src="/img/homepages-mega-menu-image-alt.jpg" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,8 +26,19 @@ export default {
 .ms_menu{
   top: 200px;
   right: 0;
-  padding: 10px;
+  padding: 20px;
   background: $bgMainColor;
   color: $textPrimaryColor;
+  z-index: 1;
+  i{
+    cursor: pointer;
+    transition: .5s;
+    &:hover{
+      color: $brandColor;
+    }
+  }
+  &:hover .col-10{
+    display: block!important;
+  }
 }
 </style>
