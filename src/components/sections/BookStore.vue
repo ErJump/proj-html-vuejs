46879fb2847764f486f7fd1ce7823e967e4a0108
ErJump <fedelijunior@gmail.com>
2022-07-27 10:17:50 +0200

    feat: :sparkles: Adds clients to BookStore

--- src/components/sections/BookStore.vue
+++ src/components/sections/BookStore.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-lg pt-5">
-    <div class="row">
+    <div class="row mb-5">
       <div class="col-5">
         <h5 class="text-uppercase ms_secondary_color fs-6 mb-4">book store online</h5>
         <h3 class="ms_font_family fs-1 ms_primary_color mb-4">
@@ -21,32 +21,68 @@
         </div>
       </div>
     </div>
+    <div class="row align-items-center pt-5">
+      <div v-for="client in clients" :key="client.id" class="col-2 ms_client">
+        <img :src="completeImgPath(client.img)" class="img-fluid" :alt="client.img">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BookCard from "../commons/BookCard.vue";
 export default {
-    name: "BookStore",
-    components: { BookCard },
-    data: function() {
-      return {
-        books: [
-          {
-            title: "Beta man by Maxcoach",
-            img: "product-book-11-400x400.jpg",
-            price: [29,"00"],
-            id: 1,
-          },
-          {
-            title: "Fake man 4.0 by Maxcoach",
-            img: "product-book-10-400x400.jpg",
-            price: [39,"00"],
-            id: 1,
-          },
-        ]
-      }
+  name: "BookStore",
+  components: { BookCard },
+  data: function() {
+    return {
+      books: [
+        {
+          title: "Beta man by Maxcoach",
+          img: "product-book-11-400x400.jpg",
+          price: [29,"00"],
+          id: 1,
+        },
+        {
+          title: "Fake man 4.0 by Maxcoach",
+          img: "product-book-10-400x400.jpg",
+          price: [39,"00"],
+          id: 2,
+        },
+      ],
+      clients: [
+        {
+          id: 1, 
+          img: "client-logo-05-primary.png",
+        },
+        {
+          id: 2, 
+          img: "client-logo-06-primary.png",
+        },
+        {
+          id: 3, 
+          img: "client-logo-07-primary.png",
+        },
+        {
+          id: 4, 
+          img: "client-logo-04-primary.png",
+        },
+        {
+          id: 5, 
+          img: "client-logo-03-primary.png",
+        },
+        {
+          id: 6, 
+          img: "client-logo-02-primary.png",
+        },
+      ]
     }
+  },
+  methods: {
+    completeImgPath: function(img) {
+      return `/img/${img}`;
+    },
+  },
 }
 </script>
 
@@ -63,5 +99,9 @@ ul{
 }
 button.ms_button{
   width: 60%;
+}
+.ms_client{
+  filter: grayscale(1);
+  opacity: .4;
 }
 </style>
